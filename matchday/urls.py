71300894ea3215b00823e1c756711962e07cd63c
Frank Carnevale <fjcarnevale@gmail.com>
2015-04-06 21:24:20 -0400

    Basic league view as index

--- matchday/urls.py
+++ matchday/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'matchday.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^sports/', include('sports.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- sports/views.py
+++ sports/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import League,Team,Match
 
 # Create your views here.
+
+def index(request):
+  leagues = League.objects.all()
+  context = {'leagues':leagues}
+  return render(request, 'sports/index.html', context)
